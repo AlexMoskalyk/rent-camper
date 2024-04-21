3e49f17dca51ee11fbeb4539a1448a731772c37f
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-04-21 18:57:28 +0200

    added selectors

---
+++ src/store/cars/selectors.js
@@ -0,0 +1,5 @@
+export const selectCars = state => state.cars.cars;
+export const selectFavoriteCars = state => state.cars.favorites;
+export const selectIsLoading = state => state.cars.isLoading;
+export const selectError = state => state.cars.isLoading;
+export const selectCarById = state => state.cars.car;
